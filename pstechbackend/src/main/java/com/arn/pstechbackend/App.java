a5bb2f0035e7634836f6c465ce99ce26487b6962
Alok <alok.r.nayak@gmail.com>
2019-01-10 21:52:04 +0530

    My project commited succesfully.

---
+++ pstechbackend/src/main/java/com/arn/pstechbackend/App.java
@@ -0,0 +1,13 @@
+package com.arn.pstechbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
